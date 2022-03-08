784e60c119674b16d32ffff4c40e03cf9178e8b9
Hussein Omar <husseinomar6190@gmail.com>
2022-03-08 22:54:35 +0300

    making changes to speed of the speech

--- app.js
+++ app.js
@@ -3,23 +3,30 @@ const pauseButton = document.getElementById('pause-button');
 const stopButton = document.getElementById('stop-button');
 const textInput = document.getElementById('text');
 const speedInput = document.getElementById('speed');
+let currentCharacter;
 
 // Event Listeners
 playButton.addEventListener('click', () => {
   playText(textInput.value);
 });
-
 pauseButton.addEventListener('click', pauseText);
-
 stopButton.addEventListener('click', stopText);
+speedInput.addEventListener('input', () => {
+  stopText();
+  playText(utterance.text.substring(currentCharacter));
+});
+
+const utterance = new SpeechSynthesisUtterance();
+utterance.addEventListener('boundary', (e) => {
+  currentCharacter = e.charIndex;
+});
 
 function playText(text) {
+  // API we use is SpeechSynthesis
   if (speechSynthesis.paused && speechSynthesis.speaking) {
     return speechSynthesis.resume();
   }
-  // API we use is SpeechSynthesis
-  const utterance = new SpeechSynthesisUtterance(text);
-  // console.log(utterance);
+  utterance.text = text;
   utterance.rate = speedInput.value || 1;
   textInput.disabled = true;
   utterance.addEventListener('end', () => {
