641dd0279182e4769cd215ad782ea43b6c8a6616
Hussein Omar <husseinomar6190@gmail.com>
2022-03-08 22:08:33 +0300

    making the pause and stop buttons function

--- app.js
+++ app.js
@@ -4,18 +4,35 @@ const stopButton = document.getElementById('stop-button');
 const textInput = document.getElementById('text');
 const speedInput = document.getElementById('speed');
 
+// Event Listeners
 playButton.addEventListener('click', () => {
   playText(textInput.value);
 });
 
+pauseButton.addEventListener('click', pauseText);
+
+stopButton.addEventListener('click', stopText);
+
 function playText(text) {
+  if (speechSynthesis.paused && speechSynthesis.speaking) {
+    return speechSynthesis.resume();
+  }
   // API we use is SpeechSynthesis
   const utterance = new SpeechSynthesisUtterance(text);
-  console.log(utterance);
+  // console.log(utterance);
   utterance.rate = speedInput.value || 1;
   textInput.disabled = true;
   utterance.addEventListener('end', () => {
     textInput.disabled = false;
   });
   speechSynthesis.speak(utterance);
+}
+
+function pauseText() {
+  if (speechSynthesis.speaking) speechSynthesis.pause();
+}
+
+function stopText() {
+  speechSynthesis.resume();
+  speechSynthesis.cancel();
 }
